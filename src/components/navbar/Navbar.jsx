5646bfa225229ff31f9c633780e6d5b6adfd8d01
Frank Awani <awanifrank@gmail.com>
2023-01-23 18:09:18 +0100

    still working on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "./navbar.scss";
-import { SearchOutlined } from "@mui/icons-material";
+import {
+  SearchOutlined,
+  LanguageOutlined,
+  DarkModeOutlined,
+  FullscreenExitOutlined,
+  NotificationsNoneOutlined,
+  ChatBubbleOutlineOutlined,
+  ListOutlined,
+} from "@mui/icons-material";
 function Navbar() {
   return (
     <div className="navbar">
@@ -8,6 +16,27 @@ function Navbar() {
         <div className="search">
           <input type="text" placeholder="search..." />
           <SearchOutlined />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlined />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlined />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlined />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlined />
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlined />
+          </div>{" "}
+          <div className="item">
+            <ListOutlined />
+          </div>
         </div>
       </div>
     </div>
