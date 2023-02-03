a7468c4089489501ee2a344f0b87ec0595e8834f
Frank Awani <awanifrank@gmail.com>
2023-02-03 16:43:29 +0100

    push

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -12,17 +12,17 @@ function List() {
     {
       id: 1143155,
       product: "Acer Nitro 5",
-      img: "https://unsplash.com/photos/K84vnnzxmTQ",
+      img: "https://technextgroup.com/wp-content/uploads/2022/07/Top-5-Apple-Products-to-buy-at-Tech-Next-store.jpg",
       customer: "John Doe",
       date: "1 March",
       amount: 785,
       method: "Cash on Delivery",
-      status: "Appoved",
+      status: "Approved",
     },
     {
       id: 2235235,
       product: "Playstation 5",
-      img: "https://unsplash.com/photos/Ba1eGcAFj5w",
+      img: "https://images.unsplash.com/photo-1491933382434-500287f9b54b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXBwbGUlMjBwcm9kdWN0fGVufDB8fDB8fA%3D%3D&w=1000&q=80",
       customer: "Micheal Doe",
       date: "1 March",
       amount: 785,
@@ -32,32 +32,32 @@ function List() {
     {
       id: 2342352,
       product: "Redragon S101",
-      img: "https://unsplash.com/photos/K84vnnzxmTQ",
+      img: "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/trade-in-og-202205?wid=1200&hei=630&fmt=jpeg&qlt=95&.v=1649887002751",
       customer: "John Doe",
       date: "1 March",
       amount: 35,
       method: "Cash on Delivery",
-      status: "pending",
+      status: "Pending",
     },
     {
       id: 2357741,
       product: "Razer Blade 15",
-      img: "https://unsplash.com/photos/AGr13YPOPqw",
+      img: "https://images.unsplash.com/photo-1609334761848-77b4d1994040?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXBwbGUlMjBwcm9kdWN0c3xlbnwwfHwwfHw%3D&w=1000&q=80",
       customer: "Jane Doe",
       date: "1 March",
       amount: 920,
       method: "Online",
-      status: "Appoved",
+      status: "Approved",
     },
     {
       id: 234355,
       product: "ASUS ROG Strix",
-      img: "https://unsplash.com/photos/2EGNqazbAMk",
+      img: "https://www.apple.com/v/mac/home/bp/images/meta/mac__bfa414svyuc2_og.png",
       customer: "Carol Doe",
       date: "1 March",
       amount: 2000,
       method: "Online",
-      status: "pending",
+      status: "Pending",
     },
   ];
 
@@ -79,12 +79,19 @@ function List() {
           {rows.map((row) => (
             <TableRow key={row.id}>
               <TableCell className="tableCell">{row.id}</TableCell>
-              <TableCell className="tableCell"></TableCell>
+              <TableCell className="tableCell">
+                <div className="cellWrapper">
+                  <img src={row.img} alt="" className="image" />
+                  {row.product}
+                </div>
+              </TableCell>
               <TableCell className="tableCell">{row.customer}</TableCell>
               <TableCell className="tableCell">{row.date}</TableCell>
               <TableCell className="tableCell">{row.amount}</TableCell>
               <TableCell className="tableCell">{row.method}</TableCell>
-              <TableCell className="tableCell">{row.status}</TableCell>
+              <TableCell className="tableCell">
+                <span className={`status ${row.status}  `}>{row.status}</span>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
